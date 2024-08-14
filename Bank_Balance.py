230e9e415f7c376e977e95ab03c8ae2ccae976bb
sohan0031 <patilsohan0031@gmail.com>
2024-08-13 20:43:05 -0700

    Create Bank_Balance.py

---
+++ Bank_Balance.py
@@ -0,0 +1,36 @@
+class Bank_balance():
+    Account_Number = 123
+    Balance = 0
+
+    def __init__(self,Balance,Account_Number) :
+        self.Balance = Balance
+        self.Account_Number = Account_Number
+
+    def Debit(self,deb):
+        deb = int(input("Enter Amount to Debit : "))
+        self.Balance-=deb
+        print("The Account is deboted by : ", deb)
+        print("The Balance is :",self.Balance)
+        
+    def Credit(self,cred):
+        cred = int(input("Enter Amount to Credit : "))
+        self.Balance += cred
+        print("The Account is Credited by : ", cred)
+        print("The Balance is :",self.Balance)
+
+
+    def show_Balance(self) :
+        print("Your Final Balance is : ",self.Balance)
+
+
+
+Balance = int(input("Enter Account Balance : " ))
+Account_Number = int(input("Enter Account Number :"))
+
+a1 = Bank_balance(Balance ,Account_Number )
+a1.Debit(1000)
+a1.Credit(100)
+a1.show_Balance()
+
+
+    
